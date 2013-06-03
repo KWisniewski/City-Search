b544597335d8e1f6cff380c681499ea8d22a28e2
Konrad Wiśniewski <piccolo24@gmail.com>
2013-06-03 22:50:29 +0200

    Dodano: - Usuwanie użytkowników z pokoi. - Dodawanie do innego pokoju, jeżeli poprzedni jest pełen. - Poprawilono mutli-roomy

--- map/map/app.js
+++ map/map/app.js
@@ -1,13 +1,12 @@
-
 /**
  * Module dependencies.
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+    , routes = require('./routes')
+    , user = require('./routes/user')
+    , http = require('http')
+    , path = require('path');
 
 var app = express(),
     server = http.createServer(app),
@@ -26,73 +25,98 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+    app.use(express.errorHandler());
 }
 
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-io.sockets.on('connection', function (socket) {
-    socket.emit('news', {hello:'world'});
-    socket.on('other', function(data){
-        console.log(data);
-    });
-});
-
-server.listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
-
-app.listen(8080);
-
-// routing
-app.get('/', function (req, res) {
-    res.sendfile(__dirname + '/index.html');
+server.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
 });
 
 // usernames which are currently connected to the chat
 var usernames = {};
-
+var rooms = [];
+var actualRoom = 0;
 // rooms which are currently available in chat
-var rooms = ['room1','room2','room3'];
+var rooms = ['room1', 'room2', 'room3'];
+var RoomUsers = {};
+for(var i = 0; i < rooms.length; i+=1){
+    RoomUsers[rooms[i]] = {};
+}
+
+var HowMuchUsers = function(obj){
+    return Object.keys(obj).length;
+};
 
 io.sockets.on('connection', function (socket) {
 
     // when the client emits 'adduser', this listens and executes
-    socket.on('adduser', function(username){
+    socket.on('adduser', function (username) {
+
+        if(HowMuchUsers(RoomUsers[rooms[actualRoom]]) > 0){ // zrobic z tego for
+            actualRoom += 1;
+        }
+
         // store the username in the socket session for this client
         socket.username = username;
         // store the room name in the socket session for this client
-        socket.room = 'room1';
+        socket.room = rooms[actualRoom];
         // add the client's username to the global list
         usernames[username] = username;
+        RoomUsers[rooms[actualRoom]][username] = {};
         // send client to room 1
-        socket.join('room1');
+        socket.join(rooms[actualRoom]);
+
+        console.log(RoomUsers);
+
         // echo to client they've connected
         socket.emit('updatechat', 'SERVER', 'you have connected to room1');
         // echo to room 1 that a person has connected to their room
-        socket.broadcast.to('room1').emit('updatechat', 'SERVER', username + ' has connected to this room');
-        socket.emit('updaterooms', rooms, 'room1');
+        socket.broadcast.to(rooms[actualRoom]).emit('updatechat', 'SERVER', username + ' has connected to this room');
+        socket.emit('updaterooms', rooms, rooms[actualRoom]);
     });
 
-    socket.on('switchRoom', function(newroom){
-        // leave the current room (stored in session)
-        socket.leave(socket.room);
-        // join new room, received as function parameter
-        socket.join(newroom);
-        socket.emit('updatechat', 'SERVER', 'you have connected to '+ newroom);
-        // sent message to OLD room
-        socket.broadcast.to(socket.room).emit('updatechat', 'SERVER', socket.username+' has left this room');
-        // update socket session room title
-        socket.room = newroom;
-        socket.broadcast.to(newroom).emit('updatechat', 'SERVER', socket.username+' has joined this room');
-        socket.emit('updaterooms', rooms, newroom);
-    });
+//    socket.on('switchRoom', function (newroom) {
+//        // leave the current room (stored in session)
+//        socket.leave(socket.room);
+//        // join new room, received as function parameter
+//        socket.join(newroom);
+//        socket.emit('updatechat', 'SERVER', 'you have connected to ' + newroom);
+//        // sent message to OLD room
+//        socket.broadcast.to(socket.room).emit('updatechat', 'SERVER', socket.username + ' has left this room');
+//        // update socket session room title
+//        socket.room = newroom;
+//        socket.broadcast.to(newroom).emit('updatechat', 'SERVER', socket.username + ' has joined this room');
+//        socket.emit('updaterooms', rooms, newroom);
+//    });
 
     // when the user disconnects.. perform this
-    socket.on('disconnect', function(){
+    socket.on('disconnect', function () {
         // remove the username from global usernames list
+        console.log("======= Remove User from Room =======")
         delete usernames[socket.username];
+        var users, room;
+        for(var i = 0; i < rooms.length; i += 1){
+            users = Object.keys(RoomUsers[rooms[i]]);
+            console.log(users);
+            for(var u = 0; u < users.length; u += 1){
+                if(users[u] === socket.username){
+                    room = rooms[i];
+                    break;
+                }
+            }
+            if(room !== undefined){
+                break;
+            }
+        }
+        console.log('*== Removing ' + socket.username + ' from ' + room + ' ==*');
+        delete RoomUsers[room][socket.username]
+
+        console.log(RoomUsers);
+
+//        for(var i = 0; i < rooms;)
         // update list of users in chat, client-side
         io.sockets.emit('updateusers', usernames);
         // echo globally that this client has left
--- map/map/public/javascripts/map.js
+++ map/map/public/javascripts/map.js
@@ -1,8 +1,47 @@
-$(document).ready(function (){
-    console.log("on");
-    var socket = io.connect('http://localhost');
-    socket.on('news', function(data){
-        console.log(data);
-        socket.emit('other', {my: 'data'});
+var socket = io.connect('http://localhost:3001');
+// on connection to server, ask for user's name with an anonymous callback
+socket.on('connect', function () {
+    // call the server-side function 'adduser' and send one parameter (value of prompt)
+    socket.emit('adduser', prompt("What's your name?"));
+});
+// listener, whenever the server emits 'updatechat', this updates the chat body
+socket.on('updatechat', function (username, data) {
+    $('#conversation').append('<b>' + username + ':</b> ' + data + '<br>');
+});
+// listener, whenever the server emits 'updaterooms', this updates the room the client is in
+socket.on('updaterooms', function (rooms, current_room) {
+    $('#rooms').empty();
+    $.each(rooms, function (key, value) {
+        if (value == current_room) {
+            $('#rooms').append('<div>' + value + '</div>');
+        }
+        else {
+            var str = '<div><a href="#" onclick="switchRoom(\''+value+'\')">' + value + '</a></div>'
+            $('#rooms').append(str);
+        }
     });
 });
+
+function switchRoom(room) {
+    socket.emit('switchRoom', room);
+}
+
+// on load of page
+$(function () {
+    // when the client clicks SEND
+    $('#datasend').click(function () {
+        var message = $('#data').val();
+        $('#data').val('');
+        // tell server to execute 'sendchat' and send along one parameter
+        socket.emit('sendchat', message);
+    });
+    // when the client hits ENTER on their keyboard
+    $('#data').keypress(function (e) {
+        if (e.which == 13) {
+            $(this).blur();
+            $('#datasend').focus().click();
+        }
+    });
+});
+
+
